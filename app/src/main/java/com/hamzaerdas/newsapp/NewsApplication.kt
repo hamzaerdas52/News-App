4b8f89fca8dfa892d96984dd12924927d26057d1
Hamza <hamza05259@gmail.com>
2023-04-09 15:35:14 +0300

    Added hilt to gradle

---
+++ app/src/main/java/com/hamzaerdas/newsapp/NewsApplication.kt
@@ -0,0 +1,8 @@
+package com.hamzaerdas.newsapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NewsApplication: Application() {
+}
---
+++ app/src/main/java/com/hamzaerdas/newsapp/repository/NewsRepository.kt
@@ -0,0 +1,4 @@
+package com.hamzaerdas.newsapp.repository
+
+class NewsRepository {
+}
